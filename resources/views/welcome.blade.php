56f12de3fcc2a090eb1252848c27dc53d6c11fd9
Ayelén Escalante <daianaayelenescalante@gmail.com>
2025-10-04 14:35:41 -0300

    Agrega mis modificaciones locales

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('components._hero')
+    @include('components._about')
+    @include('components._facilities')
+    @include('components._activities')
+    @include('components._contact')
+    @endsection
